3a37833345f200443c8d6e70f35ca7deb43a1276
Jason Rouet <56646501+jaz-on@users.noreply.github.com>
2025-12-21 23:20:26 +0100

    Initial commit: Jardin theme foundation

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Jardin functions and definitions
+ *
+ * @package Jardin
+ * @since 0.1.0
+ */
+
+// Prevent direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Define theme constants.
+define( 'JARDIN_VERSION', '0.1.0' );
+
